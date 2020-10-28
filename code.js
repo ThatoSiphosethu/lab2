7c4398cea970e236eaa7c5a8504a2a80c13e6136
Thato Ramalepa <tramalepa@my.wctc.edu>
2020-10-27 23:03:04 -0500

    Initial commit

---
+++ code.js
@@ -0,0 +1,9 @@
+function clickNumber() {
+    var clicks = prompt("Click here to continue!");
+    var moreClicks = prompt("Are you done?");
+}
+
+function totalClicks() {
+    var totalClicks = click * moreClicks
+    alert(`You had a total of ${totalClicks}`)
+}
